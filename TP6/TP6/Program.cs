913b3f06e1d4107551eb49e1e8b9d3434324a233
Carlos-Lazo <carlos.lazo550@gmail.com>
2020-05-26 16:27:41 -0300

    Punto 1

---
+++ TP6/TP6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace TP6
+{
+    class Program
+    {   
+
+        static void Main(string[] args)
+        {
+            int num, aux = 0;
+            Console.WriteLine("Ingrese el numero");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num > 0)
+            {
+                while(num > 0)
+                {
+                    aux = aux * 10 + num % 10;
+                    num = num / 10;
+                }
+
+                Console.WriteLine("Numero invertido: " + aux);
+            }
+            else
+            {
+                Console.WriteLine("Numero negativo");
+            }
+            
+      
+           
+        }
+    }
+}
