711107f8f23c0e435580449ad13288e905c23b3f
Carlos-Lazo <carlos.lazo550@gmail.com>
2020-05-26 23:52:51 -0300

    Punto 3

---
+++ TP6/Punto3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Math;
+namespace Punto3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("Ingrese un número");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"El cuadrado de {num} es {Math.Pow(num, 2)}");
+                Console.WriteLine($"La raiz cuadrada de {num} es {Math.Sqrt(num)}");
+                Console.WriteLine($"El seno de {num} es {Math.Sin(num)}");
+                Console.WriteLine($"El coseno de {num} es {Math.Cos(num)}");
+               
+            
+                Console.WriteLine("Ingrese un número con , flotante");
+                double numF = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"La parte entera de {numF} es {Math.Truncate(numF)}");
+                
+                Console.WriteLine("Ingrese un número");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+               
+                Console.WriteLine("Ingrese otro número");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"El máximo entre {num1} y {num2} es: {Math.Max(num1, num2)}");
+                Console.WriteLine($"El mínimo entre {num1} y {num2} es: {Math.Min(num1, num2)}");
+            
+        }
+    }
+}
